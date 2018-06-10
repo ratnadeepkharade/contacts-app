dcbf3247e0b6b793cef9f92a5977952ec0628942
ratnadeepkharade <ratnadeepkharade@gmail.com>
2018-06-10 18:46:58 +0530

    added add contact component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactsComponent } from './contacts/contacts.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/contacts', pathMatch: 'full' },
-  { path : 'contacts', component : ContactsComponent }
+  { path : 'contacts', component : ContactsComponent },
+  { path : 'add-contact', component : AddContactComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponent
+    ContactsComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
